dd0757707b37a9a1943d1a4806303a59a4511ccd
Nick Landolfi <nclandolfi@gmail.com>
2015-04-25 10:50:31 -0700

    add common template funcs

---
+++ templates/funcs.go
@@ -0,0 +1,43 @@
+package templates
+
+import (
+	"errors"
+	"fmt"
+	"html/template"
+)
+
+type Interpolator func(string) string
+type HTMLInterpolator func(string) template.HTML
+
+var (
+	Dict = func(vals ...interface{}) (map[string]interface{}, error) {
+		if len(vals)%2 != 0 {
+			return nil, errors.New("Must pass element pairs")
+		}
+
+		dict := make(map[string]interface{}, len(vals)/2)
+
+		for i := 0; i < len(vals); i += 2 {
+			key, ok := vals[i].(string)
+			if !ok {
+				return nil, errors.New("Keys must be strings")
+			}
+
+			dict[key] = vals[i+1]
+		}
+
+		return dict, nil
+	}
+
+	CharSet = func(s string) template.HTML {
+		return template.HTML(fmt.Sprintf("<meta charset=\"%s\">", s))
+	}
+
+	UTF8 = func() template.HTML {
+		return CharSet("utf-8")
+	}
+
+	CSS = func(href string) template.HTML {
+		return template.HTML(fmt.Sprintf("<link rel=\"stylesheet\" type=\"text/css\" href=\"%s\">", href))
+	}
+)
