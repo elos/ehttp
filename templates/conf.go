9335c0a3dc18ce27214d72496c502025e16a4dea
Nick Landolfi <nclandolfi@gmail.com>
2015-05-18 17:52:55 -0700

    add comments

--- templates/conf.go
+++ templates/conf.go
@@ -2,16 +2,14 @@ package templates
 
 import "go/build"
 
+// Name is the idiomatic mannerto refer to a TemplateSet
 type Name int
 
-/*
-	PackagePath finds the full path for the specified
-	golang import path
-
-	i.e. PackagePath("github.com/elos/httpserver/templates")
-	     -> "~/Nick/workspace/go/src/github.com/elos/httpserver/templates"
-			or some equivlent root path
-*/
+// PackagePath finds the full path for the specified
+// golang import path
+//
+// i.e., PackPath("github.com/elos/ehttp")
+// => "~/Nick/workspace/go/src/github.com/elos/ehttp" (on my computer)
 func PackagePath(importPath string) string {
 	p, err := build.Default.Import(importPath, "", build.FindOnly)
 	if err != nil {
@@ -20,17 +18,15 @@ func PackagePath(importPath string) string {
 	return p.Dir
 }
 
-/*
-	Prepend creates a slice of strings from variadic
-	arguments with the guarantee that the slice will be
-	of size >= 1, with index 0 equal to s
-
-	Prepend is useful for constructing templateSets
-	i.e.,
-		func Root(v ...string) []string {
-			return Prepend("root.tmpl", v...)
-		}
-*/
+// Prepend creates a slice of string from variadic
+// arguments with the guarantee that the slice will
+// be of size >= 1, with index 0 equal to s
+//
+// Prepend is useful for constructing templateSets
+// i.e.,
+//		func Root(v ...string) []string {
+//			return Prepend("root.tmpl", v...)
+//		}
 func Prepend(s string, v ...string) []string {
 	l := make([]string, len(v)+1)
 	l[0] = s
--- templates/engine.go
+++ templates/engine.go
@@ -7,26 +7,54 @@ import (
 )
 
 type (
+	// TemplateSet is the type for a set of templates, associated
+	// from name to a a slice of file names from which to load the template
 	TemplateSet map[Name][]string
 
-	TemplateMap map[Name]*template.Template
+	// templateMap is the type for a map of template names
+	// to their actual loaded templates (used internally to engine)
+	templateMap map[Name]*template.Template
 
+	// Context represents any structure that conserve as the global
+	// context for an egnine. The WithData(interface{}) is used
+	// so that the context can deal with the data passed into the
+	// execution of the template in any way it likes
 	Context interface {
 		WithData(interface{}) Context
 	}
 
+	// An Engine is the primary feature of the templates package,
+	// it is considered an immutable structure which can render
+	// templates.
 	Engine struct {
-		rootDir       string
-		tset          *TemplateSet
-		tmap          *TemplateMap
-		fmap          template.FuncMap
+		// Root directory from which to look up relative file names
+		// in all template sets
+		rootDir string
+
+		// The user provided TemplateSet definition for templates
+		tset *TemplateSet
+
+		// The currently generated templateMap
+		tmap *templateMap
+
+		// The map of functions supposed to be applied
+		// to each of the functions
+		fmap template.FuncMap
+
+		// The context which wraps whatever interface{} the template
+		// is executed with
 		globalContext Context
-		everyload     bool
+
+		// A flag indicating whether to reload the templates every time
+		// they are executed
+		everyload bool
 	}
 )
 
+// NewEngine instantiates a new templates.Engine, rooted at the
+// provided rootDir and based on the templateSet definition tset
 func NewEngine(rootDir string, tset *TemplateSet) *Engine {
-	tm := make(TemplateMap)
+	tm := make(templateMap)
 
 	return &Engine{
 		rootDir: rootDir,
@@ -35,6 +63,8 @@ func NewEngine(rootDir string, tset *TemplateSet) *Engine {
 	}
 }
 
+// WithContext constructs a new templates.Engine based on the
+// current tempaltes.Engine, e, but with the context provided, c.
 func (e *Engine) WithContext(c Context) *Engine {
 	return &Engine{
 		rootDir:       e.rootDir,
@@ -46,6 +76,8 @@ func (e *Engine) WithContext(c Context) *Engine {
 	}
 }
 
+// WithEveryLoad constructs a new templates.Engine based on the
+// current templates.Engine, e, but with everload set to true.
 func (e *Engine) WithEveryLoad() *Engine {
 	return &Engine{
 		rootDir:       e.rootDir,
@@ -57,6 +89,8 @@ func (e *Engine) WithEveryLoad() *Engine {
 	}
 }
 
+// WithFuncMap constructs a new templates.Engine based on the
+// current templates.Engine, e, but with the function map fm
 func (e *Engine) WithFuncMap(fm template.FuncMap) *Engine {
 	return &Engine{
 		rootDir:       e.rootDir,
@@ -68,6 +102,15 @@ func (e *Engine) WithFuncMap(fm template.FuncMap) *Engine {
 	}
 }
 
+// Execute executes the template referenced by name, with the global context
+// wrapped data, and writes the output to the io.Writer, w. If everyload is
+// set to true it first re-parses ALL the templates. if the template
+// is not defined, it returns a templates.NotFoundError
+// If the globalContext is nil, it doesn't wrap the provided data interface in
+// anything. If there is an error while rendering the template, it returns a
+// templates.RenderError
+// If everyload is true, the client can also recieve any error that the Parse()
+// function of this engine may return.
 func (e *Engine) Execute(w io.Writer, name Name, data interface{}) error {
 	// everyload is like a development mode, always re-compile templates
 	if e.everyload {
@@ -77,27 +120,37 @@ func (e *Engine) Execute(w io.Writer, name Name, data interface{}) error {
 		}
 	}
 
+	// retrieve template by name
 	t, ok := (*e.tmap)[name]
 
-	if !ok {
+	if !ok { // the name is undefined
 		return NewNotFoundError(name)
 	}
 
+	// if we have a global context, wrap our data in it
 	if e.globalContext != nil {
 		data = e.globalContext.WithData(data)
 	}
 
+	// execute, writing to w, with data
 	if err := t.Execute(w, data); err != nil {
+		// execution error
 		return NewRenderError(err)
 	}
 
+	// success
 	return nil
 }
 
-// Must be executed at least once to load templates, if the template set changes post-hoc
-// you must Parse() again to see the changes
-// The requirement engine specifies on templates is that each set define a "ROOT" element
+// Parse loads the templates defined on e.tset, into the internal template. It must
+// be executed at least once to load templates. If the template set changes post-hoc
+// which it can (because the client should still hold a pointer to it), the client must
+// Parse() again to see the changed.
+// The templates package imposed requirement on all templates loaded is that they have a "ROOT"
+// element - this handles which template to load without needing to synchronize template.Names with
+// {{define "TemplateName"}}s
 func (e *Engine) Parse() error {
+	// For each of the template sets
 	for name, set := range *e.tset {
 		t := template.New("")
 
@@ -109,6 +162,7 @@ func (e *Engine) Parse() error {
 			return err
 		}
 
+		// look for the special ROOT template
 		t = t.Lookup("ROOT")
 		if t == nil {
 			return fmt.Errorf("ROOT template not found in %v", set)
