dcd04b03d51f3ffd1bf31fcc584b2c95772128ee
Nick Landolfi <nclandolfi@gmail.com>
2016-01-13 15:17:09 -0800

    some comments on routing

--- serve/router.go
+++ serve/router.go
@@ -2,23 +2,26 @@ package serve
 
 import "net/http"
 
-type Route func(c *Conn)
+type (
+	// Route is the serve equivalent of an http.Handler
+	Route func(c *Conn)
+
+	Router interface {
+		http.Handler
+
+		DELETE(string, Route)
+		GET(string, Route)
+		HEAD(string, Route)
+		OPTIONS(string, Route)
+		POST(string, Route)
+		PATCH(string, Route)
+		PUT(string, Route)
+		ServeFiles(string, http.FileSystem)
+	}
+)
 
 func Handler(route Route) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		route(NewConn(w, r, nil))
 	})
-}
-
-type Router interface {
-	http.Handler
-
-	DELETE(string, Route)
-	GET(string, Route)
-	HEAD(string, Route)
-	OPTIONS(string, Route)
-	POST(string, Route)
-	PATCH(string, Route)
-	PUT(string, Route)
-	ServeFiles(string, http.FileSystem)
 }
